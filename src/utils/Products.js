bac5ce4353896ff4a98fcc0587d153d310b57e90
Felipe Lima <felipe.lima@corebiz.com.br>
2019-07-19 14:34:39 -0300

    alter data of mock

--- src/utils/Products.js
+++ src/utils/Products.js
@@ -5,34 +5,34 @@ module.exports = {
   load: function() {
     return [
       {
-        id : '00001',
-        sku: 123456,
-        name: 'Brooklyn Lager',
-        image: 'http://images.meredith.com/rrmag/images/2012/05/ss_BK-Lager.jpg',
+        id : '1',
+        sku: 1,
+        name: 'Cerveja Skol',
+        image: '',
         price: 10.99,
         inventory: 6
       },
       {
-        id : '00002',
-        sku: 123234,
-        name: 'Abita Purple Haze',
-        image: 'http://images.meredith.com/rrmag/images/2012/05/ss_Abita-Purple-Haze.jpg',
+        id : '2',
+        sku: 2,
+        name: 'Cerveja Brahma',
+        image: '',
         price: 7.99,
         inventory: 2
       },
       {
-        id : '00003',
-        sku: 1236789,
-        name: 'Miller High Life',
-        image: 'http://images.meredith.com/rrmag/images/2012/05/ss_Miller.jpg',
+        id : '3',
+        sku: 3,
+        name: 'Cerveja Itaipava',
+        image: '',
         price: 4.89,
         inventory: 12
       },
       {
-        id : '00004',
-        sku: 2346430,
-        name: 'Samuel Adams Boston Lager',
-        image: 'http://images.meredith.com/rrmag/images/2012/05/ss_Sam-Adams.jpg',
+        id : '4',
+        sku: 4,
+        name: 'Cerveja Sol',
+        image: '',
         price: 6.99,
         inventory: 4
       },
